a1a3010b3a3a501b4d3015d012662884db615fa9
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-22 03:21:18 +0700

    Drop iojs and node 0.x support

--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,8 @@ var expect = require('chai').expect;
 var duration = require('../src').duration;
 var modifiedDuration  = require('../src').modifiedDuration;
 
-describe('Duration calculation', function() {
-  it('calculate Macaulay Duration correctly', function() {
+describe('Duration calculation', () => {
+  it('calculate Macaulay Duration correctly', () => {
     const faceValue = 10000;
     const couponRate = 0.05;
     const maturity = 5;
@@ -14,7 +14,7 @@ describe('Duration calculation', function() {
     expect(years).to.equal(4.545950504162359);
   });
 
-  it('calculate Modified Duration correctly', function() {
+  it('calculate Modified Duration correctly', () => {
     const faceValue = 10000;
     const couponRate = 0.05;
     const maturity = 5;
