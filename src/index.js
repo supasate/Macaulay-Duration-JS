649c403b3f8bf1eb86729e9c174b6059361073ef
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-22 02:35:58 +0700

    Implement Modified Duration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-exports.duration = function(faceValue, couponRate, maturity) {
+var duration = function(faceValue, couponRate, maturity) {
   let time;
   let sum = 0;
+
   for (time = 1; time <= maturity; time++) {
-    let cashFlow = faceValue * couponRate
+    let cashFlow = faceValue * couponRate;
+
     if (time === maturity) {
       cashFlow += faceValue;
     }
-    
+
     const presentValue = cashFlow / Math.pow(1 + couponRate, time);
     const weight = presentValue / faceValue;
     sum += weight * time;
   }
 
   return sum;
-}
+};
+
+var modifiedDuration = function(faceValue, couponRate, maturity) {
+  const macaulayDuration = duration(faceValue, couponRate, maturity);
+  const yieldToMaturity = couponRate;
+
+  return macaulayDuration / (1 + (yieldToMaturity / maturity));
+};
+
+module.exports = {
+  duration: duration,
+  modifiedDuration: modifiedDuration
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var duration = require('../src').duration;
+var modifiedDuration  = require('../src').modifiedDuration;
 
 describe('Duration calculation', () => {
   it('calculate Macaulay Duration correctly', () => {
@@ -9,5 +10,14 @@ describe('Duration calculation', () => {
     const years = duration(faceValue, couponRate, maturity);
 
     expect(years).to.equal(4.545950504162359);
+  });
+
+  it('calculate Modified Duration correctly', () => {
+    const faceValue = 10000;
+    const couponRate = 0.05;
+    const maturity = 5;
+    const years = modifiedDuration(faceValue, couponRate, maturity);
+
+    expect(years).to.equal(4.500941093230058);
   });
 });
